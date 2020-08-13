909296303755d27fa25516676e54d00708095c2c
andrescjp <andres@cjp.mx>
2020-08-13 11:26:25 -0500

    tests

---
+++ tests/SentimentAnalysisTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Andresdevr\MeaningCloud\Tests;
+
+use Andresdevr\MeaningCloud\Facades\SentimentAnalysis;
+use Orchestra\Testbench\TestCase;
+
+class SentimentAnalysisTest extends TestCase  
+{
+    /**
+     * @test
+     */
+    function constructor_loads_config_key()
+    {
+        
+    } 
+}
