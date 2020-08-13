b3263622463817c289fe36c22c038a6a175e8123
andrescjp <andres@cjp.mx>
2020-08-13 10:49:00 -0500

    clase pincipal cargada

---
+++ src/MeaningCloud.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Andresdevr\SentimentAnalysisMeaningCloud;
+
+class MeaningCloud
+{
+    private $key;
+
+    public function __construct()
+    {
+        $this->key = "";
+    }
+
+    public function getAnalysis()
+    {
+    
+    }
+}
